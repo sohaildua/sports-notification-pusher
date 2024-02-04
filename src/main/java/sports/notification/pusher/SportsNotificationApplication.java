9952c119be479090e99cea4f50be7b5009b13b3a
dua <sohail.dua@webfleet.com>
2024-02-05 00:52:26 +0100

    refactor: making arguments to final

--- src/main/java/sports/notification/pusher/SportsNotificationApplication.java
+++ src/main/java/sports/notification/pusher/SportsNotificationApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SportsNotificationApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(SportsNotificationApplication.class, args);
 	}
 
