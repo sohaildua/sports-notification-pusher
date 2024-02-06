b9e948402f61610f86deb92e65b9955797e36402
dua <sohail.dua@webfleet.com>
2024-02-06 23:31:01 +0100

    creating base for push notification in spring boot 3

---
+++ src/main/java/sports/notification/pusher/SportsNotificationApplication.java
@@ -0,0 +1,13 @@
+package sports.notification.pusher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sports/notification/pusher/SportsNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package sports.notification.pusher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
