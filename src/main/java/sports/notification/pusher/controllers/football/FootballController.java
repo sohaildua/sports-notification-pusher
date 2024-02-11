f1c72b36505537c87dd039757202af2e343e5d36
dua <sohaildua38@gmail.com>
2024-02-11 22:53:12 +0100

    feat(football league): creating controller

--- src/main/java/sports/notification/pusher/controllers/football/FootballController.java
+++ src/main/java/sports/notification/pusher/controllers/football/FootballController.java
@@ -1,31 +1,42 @@
 package sports.notification.pusher.controllers.football;
 
-import java.time.Duration;
-import java.util.List;
-
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import sports.notification.pusher.models.footballleague.FootballDataResponse;
 
 @RestController
 @RequestMapping("/football")
 public class FootballController
 {
     private static final String LEAGUES_LIST_URL = "http://api.football-data.org/v4/competitions/";
+    private final Logger logger = LoggerFactory.getLogger(FootballController.class);
+    private final RestTemplate restTemplate;
 
     public FootballController(final RestTemplate restTemplate)
     {
+        this.restTemplate = restTemplate;
     }
 
-    // call the function and make it to football league
     @GetMapping("/leagues")
-    public List<String> getFootballLeagues(){
-        List<String> list = null;
-                return list;
+    public ResponseEntity<?> getFootballLeagues() {
+        try {
+            final FootballDataResponse response = restTemplate.getForObject(LEAGUES_LIST_URL, FootballDataResponse.class);
+            logger.debug("Response Entity: {}", response);
+            return ResponseEntity.ok(response);
+        } catch (final RestClientException e) {
+            final String errorMessage = "Error fetching football leagues: " + e.getMessage();
+            logger.error(errorMessage);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        }
     }
-
 }
+
 
