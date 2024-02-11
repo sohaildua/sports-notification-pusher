5d4a2898efc5c852404519b337be084fd9ce4058
dua <sohaildua38@gmail.com>
2024-02-11 22:52:03 +0100

    refractor(football league): delete FootballLeague.java

--- src/main/java/sports/notification/pusher/models/FootballLeague.java
+++
@@ -1,8 +0,0 @@
-package sports.notification.pusher.models;
-
-public record FootballLeague(long id,long areaId,String areaName, String areaCode, String areaFlag,
-                             String name,String code, String type,String emblem, String plan, long currentSeasonId,
-                             String currentSeasonStartDate, String currentSeasonEndDate, String currentSeasonCurrentMatchday,
-                             String currentSeasonWinnerm, int numberOfAvailableSeasons, String lastUpdated)
-{
-}
