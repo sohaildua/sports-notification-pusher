1e9442832b8ef239c0de44ab4fb0fb511f6cee40
dua <sohaildua38@gmail.com>
2024-02-20 23:12:02 +0100

    creating new java pojo classes for football league- FootballMatch

--- src/main/java/sports/notification/pusher/models/footballleague/FootballMatch.java
+++ src/main/java/sports/notification/pusher/models/footballleague/FootballMatch.java
@@ -1,7 +1,9 @@
 package sports.notification.pusher.models.footballleague;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record FootballMatch(
         @JsonProperty("id") int id,
         @JsonProperty("homeTeam") Team homeTeam,
